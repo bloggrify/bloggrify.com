fc9b7660707676804fd3744c9780e98c47e27ed5
hlassiege <hugo@malt.com>
2024-03-01 15:52:22 +0100

    update og:image

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
     title: 'Bloggr',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    description: 'The best place to start your blog.',
+    image: 'https://bloggr.eventuallycoding.com/cover.png',
     socials: {
       twitter: 'hugolassiege',
       github: 'hlassiege/bloggr',
