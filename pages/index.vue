0bbea922c324b2dbac8e56cea0b8b5ae041a5e17
hlassiege <hugo@malt.com>
2024-03-08 16:08:55 +0100

    small wording modifications

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@
 
           <div class="mt-5 max-w-3xl text-center mx-auto">
             <p class="text-lg text-gray-600 dark:text-gray-400">
-              Bloggr is a <strong>static</strong> blog engine
+              Bloggr is a <strong>static</strong> blogging platform
               built on top of Nuxt.js and Nuxt-content providing
               everything you need to start writing your blog
               hassle-free AND <strong>for free</strong>.
@@ -206,7 +206,7 @@
               </div>
             </div>
             <p class="text-gray-600 dark:text-gray-400">
-              This is a static blog engine, you can host it for
+              This is a static blogging platform, you can host it for
               free on Vercel, Netlify, Cloudflare-pages or GitHub
               Pages.
             </p>
