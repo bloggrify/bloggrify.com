5e90fb0ccd18f94b555bfdd176f29ce4f65c0282
hlassiege <hugo@malt.com>
2024-05-20 12:11:44 +0200

    Add documentation about theming system, tags and categories and a dedicated page to list all templates

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,7 @@
           </div>
 
           <!-- Buttons -->
-          <div class="mt-8 gap-8 flex justify-center">
+          <div class="mt-8 gap-4 flex justify-center">
             <NuxtLink
               class="inline-flex justify-center items-center text-xl gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
               to="/introduction/introduction"
@@ -88,18 +88,6 @@
                 class="flex justify-center items-center size-7"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="transition-transform hover:scale-110 w-6 h-6" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" /></svg>
-              </span>
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/hlassiege/bloggrify"
-              class="relative group p-2 ps-3 inline-flex items-center gap-x-2 text-lg font-mono rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            >
-              Github
-              <span
-                class="flex justify-center items-center size-7"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" class="transition-transform hover:scale-110 w-6 h-6" fill="currentColor" viewBox="0 0 496 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg>
               </span>
             </a>
           </div>
@@ -445,76 +433,152 @@
       <!-- End Icon Blocks -->
     </div>
 
-    <div class="mx-auto max-w-[85rem] md:p-4 mt-20 md:mt-40 mb-20">
-      <div class="flex mb-14 ml-10 md:ml-0">
-        <span
-          id="templates"
-          class="block font-bold text-gray-600 text-3xl md:text-4xl lg:text-5xl dark:text-gray-200"
-        >
-          Templates
-        </span>
-      </div>
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 gap-8 lg:grid-cols-3 xl:grid-cols-4"
-      >
-        <div
-          v-for="template in templates"
-          :key="template.name"
-          class="shadow-lg mx-20 md:mx-0 rounded-xl divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 bg-white dark:bg-gray-900 overflow-hidden"
-        >
-          <div class="h-48 overflow-hidden">
-            <img
-              :src="template.image"
-              width="280"
-              height="140"
-              :alt="template.name"
-              class="object-cover object-top w-full h-full"
-            >
-          </div>
-          <div class="flex-1 px-4 py-5 sm:p-6 !p-4">
-            <p
-              class="text-gray-900 dark:text-white text-base font-semibold truncate flex items-center gap-1.5"
-            >
-              <span class="flex-1 truncate">{{
-                template.name
-              }}</span>
-            </p>
-            <div
-              class="inline-flex -space-x-px rounded-md shadow-sm mt-3 w-full"
-            >
-              <a
-                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-s-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"
-                :href="template.demo"
-                rel="noopener noreferrer"
-                target="_blank"
-              ><span
-                class="i-ph-desktop-duotone flex-shrink-0 w-4 h-4"
-                aria-hidden="true"
-              /><span class="">Demo</span></a>
-              <a
-                v-if="template.repository"
-                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"
-                :href="template.repository"
-                rel="noopener noreferrer"
-                target="_blank"
-              ><span class="">GitHub</span></a>
-              <a
-                v-if="template.shop"
-                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"
-                :href="template.shop"
-                rel="noopener noreferrer"
-                target="_blank"
-              ><span class="">Buy</span></a>
-              <span
-                v-if="!template.shop && !template.repository"
-                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"
-                rel="noopener noreferrer"
-              >Soon</span>
+    <!-- Features -->
+    <div id="templates" class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <!-- Grid -->
+      <div class="md:grid md:grid-cols-2 md:gap-12 xl:gap-32 ">
+        <div class="relative h-[360px]">
+          <div class="rounded-2xl absolute top-0 left-0 bg-gray-800 border-gray-300 border h-[343px] w-[500px] rotate-3" />
+          <div class="rounded-2xl absolute top-0 left-0 bg-gray-800 border-gray-300 border h-[343px] w-[500px] -rotate-3" />
+          <div class="rounded-2xl absolute top-0 left-0 bg-gray-800 border-gray-300 border h-[343px] w-[500px] -rotate-2" />
+          <div class="rounded-2xl absolute top-0 left-0 bg-gray-800 border-gray-300 border h-[343px] w-[500px] -rotate-6" />
+          <img class="absolute top-0 left-0 rounded-xl" src="/images/templates/bloggr.png" alt="Mistral template">
+        </div>
+        <!-- End Col -->
+
+        <div class="mt-10 lg:mt-0">
+          <div class="space-y-6 sm:space-y-8">
+            <!-- Title -->
+            <div class="space-y-2 md:space-y-4">
+              <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-neutral-200">
+                Mutiple templates available
+              </h2>
+              <p class="text-gray-500 dark:text-neutral-500">
+                Choose from a variety of templates to start your blog
+                with a beautiful design.
+              </p>
             </div>
+            <!-- End Title -->
+
+            <!-- List -->
+            <ul class="space-y-2 sm:space-y-4">
+              <li class="flex space-x-3">
+                <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                  <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+                </span>
+
+                <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                  <span class="font-bold">Easy</span> to use
+                </span>
+              </li>
+
+              <li class="flex space-x-3">
+                <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                  <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+                </span>
+
+                <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                  <span class="font-bold">customizable</span>
+                </span>
+              </li>
+
+              <li class="flex space-x-3">
+                <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                  <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+                </span>
+
+                <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                  You can build
+                  <NuxtLink class="font-bold underline decoration-dashed underline-offset-4" to="/reference/theming">your own template</NuxtLink>
+                </span>
+              </li>
+            </ul>
+            <!-- End List -->
+            <NuxtLink
+              class="inline-flex justify-center items-center text-lg gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
+              to="/templates"
+            >
+              See all templates
+            </NuxtLink>
           </div>
         </div>
+        <!-- End Col -->
       </div>
+      <!-- End Grid -->
     </div>
+    <!-- End Features -->
+
+
+    <!--    <div class="mx-auto max-w-[85rem] md:p-4 mt-20 md:mt-40 mb-20">-->
+    <!--      <div class="flex mb-14 ml-10 md:ml-0">-->
+    <!--        <span-->
+    <!--          id="templates"-->
+    <!--          class="block font-bold text-gray-600 text-3xl md:text-4xl lg:text-5xl dark:text-gray-200"-->
+    <!--        >-->
+    <!--          Templates-->
+    <!--        </span>-->
+    <!--      </div>-->
+    <!--      <div-->
+    <!--        class="grid grid-cols-1 sm:grid-cols-2 gap-8 lg:grid-cols-3 xl:grid-cols-4"-->
+    <!--      >-->
+    <!--        <div-->
+    <!--          v-for="template in templates"-->
+    <!--          :key="template.name"-->
+    <!--          class="shadow-lg mx-20 md:mx-0 rounded-xl divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 bg-white dark:bg-gray-900 overflow-hidden"-->
+    <!--        >-->
+    <!--          <div class="h-48 overflow-hidden">-->
+    <!--            <img-->
+    <!--              :src="template.image"-->
+    <!--              width="280"-->
+    <!--              height="140"-->
+    <!--              :alt="template.name"-->
+    <!--              class="object-cover object-top w-full h-full"-->
+    <!--            >-->
+    <!--          </div>-->
+    <!--          <div class="flex-1 px-4 py-5 sm:p-6 !p-4">-->
+    <!--            <p-->
+    <!--              class="text-gray-900 dark:text-white text-base font-semibold truncate flex items-center gap-1.5"-->
+    <!--            >-->
+    <!--              <span class="flex-1 truncate">{{-->
+    <!--                template.name-->
+    <!--              }}</span>-->
+    <!--            </p>-->
+    <!--            <div-->
+    <!--              class="inline-flex -space-x-px rounded-md shadow-sm mt-3 w-full"-->
+    <!--            >-->
+    <!--              <a-->
+    <!--                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-s-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"-->
+    <!--                :href="template.demo"-->
+    <!--                rel="noopener noreferrer"-->
+    <!--                target="_blank"-->
+    <!--              ><span-->
+    <!--                class="i-ph-desktop-duotone flex-shrink-0 w-4 h-4"-->
+    <!--                aria-hidden="true"-->
+    <!--              /><span class="">Demo</span></a>-->
+    <!--              <a-->
+    <!--                v-if="template.repository"-->
+    <!--                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"-->
+    <!--                :href="template.repository"-->
+    <!--                rel="noopener noreferrer"-->
+    <!--                target="_blank"-->
+    <!--              ><span class="">GitHub</span></a>-->
+    <!--              <a-->
+    <!--                v-if="template.shop"-->
+    <!--                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"-->
+    <!--                :href="template.shop"-->
+    <!--                rel="noopener noreferrer"-->
+    <!--                target="_blank"-->
+    <!--              ><span class="">Buy</span></a>-->
+    <!--              <span-->
+    <!--                v-if="!template.shop && !template.repository"-->
+    <!--                class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-e-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center w-1/2 justify-center"-->
+    <!--                rel="noopener noreferrer"-->
+    <!--              >Soon</span>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
   </main>
 </template>
 <script setup lang="ts">
